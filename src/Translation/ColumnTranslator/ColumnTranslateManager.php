8bd7718d58ddc737113a766fca85c422cd20e9c0
Alireza Salehizadeh <alirezasalehizadehco@gmail.com>
2023-09-11 00:11:59 +0330

    [1.6.x] wip

--- src/Translation/ColumnTranslator/ColumnTranslateManager.php
+++ src/Translation/ColumnTranslator/ColumnTranslateManager.php
@@ -7,27 +7,22 @@ class ColumnTranslateManager
 
     private $translator;
 
-    private $translatedColumns = [];
-
     public function __construct($translator = null)
     {
         $this->resolveTranslator($translator);
     }
-    
+
     public function translate(array $columns): array
     {
-        foreach ($columns as $column) {
-            $this->translatedColumns[] = $this->translator->setColumn($column)->make();
-        }
-        return $this->translatedColumns;
+        return array_map(fn ($column) => $this->translator->setColumn($column)->make(), $columns);
     }
-    
+
     private function resolveTranslator($translator)
     {
         $columnTranslator = new ColumnTranslator;
-    
+
         $this->translator = array_key_exists($translator, $columnTranslator->availableTranslators)
-        ? new $columnTranslator->availableTranslators[$translator]
-        : new $columnTranslator->availableTranslators["MySql"];
+            ? new $columnTranslator->availableTranslators[$translator]
+            : new $columnTranslator->availableTranslators["MySql"];
     }
 }
